1c99bc73ba3ba1d2540cbd238acfa0669180cb96
Sathvik777 <katam.sathvik@gmail.com>
2018-03-31 13:38:33 +0200

    rearrange files

--- micro-services/scheduler/scheduler/app.js
+++ micro-services/scheduler/scheduler/app.js
@@ -21,12 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -37,5 +35,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
