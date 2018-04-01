cf74c0567fde4597c017b8f920c8581aa0cbb291
Sathvik777 <katam.sathvik@gmail.com>
2018-04-01 14:19:11 +0200

    deploying two services in different ports

--- micro-services/request-server/server/server.ts
+++ micro-services/request-server/server/server.ts
@@ -3,13 +3,12 @@ import * as dotenv from 'dotenv';
 import * as bodyParser from 'body-parser';
 
 import * as homeController from './controllers/home';
-import job  from './../server/scheduler_js';
 
 dotenv.config();
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', homeController.index);
@@ -19,5 +18,18 @@ app.listen(app.get('port'), () => {
   app.get('port'), app.get('env'));
   console.log('Press CTRL-C to stop\n');
 });
+
+/**
+  POST endpoint takes Json Request body with message, time/date and channel ID.
+*/
+app.post("/schedule_it",  (request, response, next) => {
+ //TODO: add auth here
+
+ // TODO: Insert in DB
+ 
+ 
+ response.send("OK");
+});
+
 
 module.exports = app;
